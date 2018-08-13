f3e095d78fec8317df27ae9a0d320ebeda3b6af3
Unknown <kevin@rustycog.com>
2018-08-13 13:30:01 -0700

    element

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ resource "azurerm_key_vault_access_policy" "default" {
   vault_name          = "${data.azurerm_key_vault.default.name}"
   resource_group_name = "${data.azurerm_key_vault.default.resource_group_name}"
 
-  tenant_id      = "${lookup(element(var.identities, count.index),"tenant_id")}"
-  object_id      = "${lookup(element(var.identities, count.index),"object_id")}"
-  application_id = "${lookup(element(var.identities, count.index),"object_id")}"
+  tenant_id      = "${lookup(element(element(var.identities, count.index),0),"tenant_id")}"
+  object_id      = "${lookup(element(element(var.identities, count.index),0),"object_id")}"
+  application_id = "${lookup(element(element(var.identities, count.index),0),"object_id")}"
 
   key_permissions = "${var.key_permissions}"
 
